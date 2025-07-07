c22ed28d965b7121e500908df59f5e3ac1c9cbe8
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2025-07-07 12:13:18 -0400

    some patches
    This works better than before, but there are bugs in the hello
    messaging that need to be patched (and in fact are in an outstanding
    PR).

--- docker-vs/entrypoint.sh
+++ docker-vs/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-ip addr add 129.6.7.1/32 dev eth0
 
 mkdir -p /var/run/zpr
 
-exec /app/bin/vservice -c /authority/vs-config.yaml -p /authority/zpr-full-access.bin -l "[fd5a:5052::2]:5002" &
+ip tuntap add name tun9 mode tun multi_queue
+ip link set tun9 mtu 1400
+ip addr add fd5a:5052::1/32 dev tun9
+ip link set tun9 up
+
+# Docker should have started node before this.
+# Give node a few secs to startup.
+sleep 7
+
+exec /app/bin/vservice -c /authority/vs-config.yaml -p /authority/zpr-full-access.bin &
+
+# Let visa service intialize...
+# TODO: it does not need 20secs, but this is helpful for debugging the output
+sleep 20
+
+# Start adapter which will immeidately try to connect to the node.
 exec /app/bin/ph-no-uring adapter -c /authority/adapter-vs-conf.toml --debug all
